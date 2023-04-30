ba96e43bd1c82050d3104d5631ee2eac830fe6e5
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-04-30 17:53:56 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function Random(maximo) {
+  return Math.floor(Math.random() * maximo);
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+
+var excuse = document.getElementById("excuse");
+excuse.innerHTML = who[Random(who.length)] + " " + action[Random(action.length)] + " " + what[Random(what.length)] + " " + when[Random(when.length)]
+
+
+console.log(who[Random(who.length)] + " " + action[Random(action.length)] + " " + what[Random(what.length)] + " " + when[Random(when.length)] );
