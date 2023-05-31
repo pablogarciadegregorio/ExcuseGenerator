7242f6dd97e9f4fbf81172dec0775c9c006454cb
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-05-31 10:00:30 +0000

    identacion y nomenclatura

--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
-function Random(maximo) {
+function randomNumber(maximo) {
   return Math.floor(Math.random() * maximo);
 }
 
-
-
-let who = ['The dog','My grandma','His turtle','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
 var excuse = document.getElementById("excuse");
-excuse.innerHTML = who[Random(who.length)] + " " + action[Random(action.length)] + " " + what[Random(what.length)] + " " + when[Random(when.length)]
+excuse.innerHTML =
+  who[randomNumber(who.length)] +
+  " " +
+  action[randomNumber(action.length)] +
+  " " +
+  what[randomNumber(what.length)] +
+  " " +
+  when[randomNumber(when.length)];
 
-
-console.log(who[Random(who.length)] + " " + action[Random(action.length)] + " " + what[Random(what.length)] + " " + when[Random(when.length)] );
+console.log(
+  who[randomNumber(who.length)] +
+    " " +
+    action[randomNumber(action.length)] +
+    " " +
+    what[randomNumber(what.length)] +
+    " " +
+    when[randomNumber(when.length)]
+);
